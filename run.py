cbf021daee3c6efc20cc63a98f76dc1ad9651ea0
hitmank <karan.b755@gmail.com>
2018-03-03 21:33:59 -0800

    manoj update

--- run.py
+++ run.py
@@ -4,6 +4,7 @@ from flask import Flask, request
 from twilio.twiml.messaging_response import Body, Media, Message, MessagingResponse
 import requests
 import route_parser
+from twilio.twiml.voice_response import VoiceResponse, Say
 #from twilio.twiml.messaging_response import MessagingResponse
 from twilio import twiml
 import urllib
@@ -20,18 +21,33 @@ def sms_ahoy_reply():
     # Start our response
     body = request.values.get('Body', None) 
     body = body.lower()
-    from_text = re.search(r'from(.*?)to', body).group(1)
-    dest_text =  body.split("to",1)[1]
+    #from_text = re.search(r'from(.*?)to', body).group(1)
+    #dest_text =  body.split("to",1)[1]
+    grp = re.search(r'from(.*?)to(.*?)by(.*?)',body)
+    from_text = grp.group(1)
+    dest_text = grp.group(2)
+    if (grp.group(3)):  
+     mode = body.split("by",1)[1]
+    else:
+  mode = None
+    print from_text
+    print dest_text
+    print mode
+    if(mode == "walk"):
+  md = "Walking"
+    else:
+  md = "Driving"
+    print md
     response = MessagingResponse()
     message_directions = Message()
     message_mapImage = Message()
    # message.body('Store Location: 123 Easy St.')
    # message.media(request.form["MediaUrl0"])
    # response.append(message)
-    r = requests.get("http://dev.virtualearth.net/REST/V1/Routes/Driving?wp.0="+from_text+"&wp.1="+dest_text+"&optmz=distance&routeAttributes=routePath&key=AmAugBLaNYZ5kK48nDsSyH2IkXZTMgLdevV0cwJkIohl5A4hitqxiCmKhDAAq7cr")
+    r = requests.get("http://dev.virtualearth.net/REST/V1/Routes/"+md+"?wp.0="+from_text+"&wp.1="+dest_text+"&optmz=distance&routeAttributes=routePath&key=AmAugBLaNYZ5kK48nDsSyH2IkXZTMgLdevV0cwJkIohl5A4hitqxiCmKhDAAq7cr")
     data = r.json() 
-
-    message_directions.body(route_parser.extract_path((data))[:100])
+    #print data
+    message_directions.body(route_parser.extract_path((data)))
     response.append(message_directions)
     #print(str(data))
     #print str(response)
@@ -46,8 +62,8 @@ def sms_ahoy_reply():
    # message_mapImage.body('Image of the route')
     message_mapImage.media("http://159.65.68.139:5000/uploads/{}".format(name))
     response.append(message_mapImage)
-   # res1 = str(response)
-
+    #res1 = str(response) 
+    #print res1 
     return str(response)
 
 @app.route('/uploads/<filename>', methods=['GET', 'POST'])
